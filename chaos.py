89e222676ef8e3d32e9fcd1bdb7a9ad8ac9d9737
chastep <chasestephens92@gmail.com>
2017-04-22 14:44:16 -0500

    create chaos file

---
+++ chaos.py
@@ -0,0 +1,8 @@
+def main():
+  print("This program illustrates a chaotic function")
+  x = eval(input("Enter a number between 0 - 1: "))
+  for i in range(10):
+    x = 3.9 * x * (1 - x)
+    print(x)
+
+main()
