712fde6f30ee4e444f688a967ea08c73314692c8
chastep <chasestephens92@gmail.com>
2017-04-23 14:33:19 -0500

    screen reset working ok

--- python_racer/runner.py
+++ python_racer/runner.py
@@ -1,5 +1,15 @@
 import subprocess
 import time
 from die import Die
-from python_racer import PythonRacer
+# from python_racer import PythonRacer
 from reset_screen import reset_screen
+
+print("this is a test")
+time.sleep(3)
+reset_screen()
+print("this is a test2")
+time.sleep(3)
+reset_screen()
+print("this is a test3")
+time.sleep(3)
+reset_screen()
