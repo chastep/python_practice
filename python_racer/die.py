293ea581fe9034f5478c77156a8e61187a83532f
chastep <chasestephens92@gmail.com>
2017-04-23 14:17:58 -0500

    make die class

--- python_racer/die.py
+++ python_racer/die.py
@@ -1,3 +1,8 @@
-import random
+from random import randinit
 class Die:
-  def _init_(self, side_count):
+  def __init__(self, side_count):
+    self.side_count = side_count
+  # roll method for die
+  # returns random number between 1-side count
+  def roll(self):
+    return randinit(1, self.side_count)
